27d27add3d73a8d7836846a95b6f25fe582d1383
Alison Chaiken <alison@she-devel.com>
2024-12-07 13:42:47 -0800

    atomic_stack: add comments
    The purpose of writing the code was to study the implementation underlying the
    new C++ atomic functions.  Sample strace and ltrace output with the results are
    included.

--- atomic-stack.impl.hh
+++ atomic-stack.impl.hh
@@ -1,13 +1,45 @@
 // Adapted from listing 7 of
 // https://accu.org/journals/overload/32/182/teodorescu/
 
+//	$ sudo strace -vf ./atomic-stack_lib_test-clang 2>&1 | grep futex
+//	futex(0x7f070906273c, FUTEX_WAKE_PRIVATE, 2147483647) = 0
+//	futex(0x7ffc59918fe8, FUTEX_WAKE_PRIVATE, 1) = 1
+//
+//	$ sudo ltrace -C -S -f ./atomic-stack_lib_test-gcc-no_sanitize 2>&1 | grep pthread
+//	[pid 254040] pthread_mutex_init(0x55b3892163c0, nil) = 0
+//	[pid 254040] pthread_mutex_init(0x55b376b01d28, nil) = 0
+//	[pid 254040] pthread_mutex_init(0x55b389216508, nil) = 0
+//	[pid 254040] pthread_key_create(0, 0x55b376a717b5) = 0
+//	[pid 254040] pthread_mutex_init(0x55b389216628, nil) = 0
+//	[pid 254040] pthread_key_create(1, 0x55b376a717b5) = 0
+//	[pid 254040] pthread_mutex_init(0x55b3892168a8, nil) = 0
+//	[pid 254040] pthread_mutex_init(0x55b389216b00, nil) = 0
+//	[pid 254040] pthread_key_create(2, 0x55b376a717b5) = 0
+//	[pid 254040] pthread_getspecific(2)              = nil
+//	[pid 254040] pthread_key_delete(2)               = 0
+//	[pid 254040] pthread_mutex_destroy(0x55b3892163c0) = 0
+//	[pid 254040] pthread_mutex_destroy(0x55b389216b00) = 0
+//	[pid 254040] pthread_mutex_destroy(0x55b3892168a8) = 0
+//	[pid 254040] pthread_getspecific(1)              = nil
+//	[pid 254040] pthread_key_delete(1)               = 0
+//	[pid 254040] pthread_mutex_destroy(0x55b389216628) = 0
+//	[pid 254040] pthread_getspecific(0)              = nil
+//	[pid 254040] pthread_key_delete(0)               = 0
+//	[pid 254040] pthread_mutex_destroy(0x55b389216508) = 0
+
+
 #include <atomic>
 
 namespace atomic_stack {
 
 template <typename T> struct node {
   T data;
-  // Could next be a unique_ptr and still work with head_ as atomic type?
+  //  "The reader might remark that next_ is not an atomic type. It doesn’t need
+  //  to be, as we always interact with it through the head_ pointer."
+  //
+  // next must be a raw pointer since atomic types must be trivially copyable.
+  // Obviously a unique_ptr is not copyable at all, while shared_ptr is
+  // copyable, but not trivially.
   node *next;
   node(const T &val) : data(val), next(nullptr) {}
 };
